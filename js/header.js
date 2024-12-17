1006eb75fd86ee9e3951e429c421d03d78c71b88
MOEN ERAK <raknosmoke000@gmail.com>
2024-12-17 20:32:43 +0700

    navtabAndPlacholder

--- js/header.js
+++ js/header.js
@@ -10,3 +10,23 @@
         console.error('Error fetching header:', error);
     }
 })();
+
+
+function navbarActive(child) {
+    var attempts = 0;
+    var interval = setInterval(function() {
+        var navMenu = document.querySelector('.navbar-menu');
+        if (navMenu) {
+            var links = navMenu.querySelectorAll('a');
+            if (links[child]) {
+                links[child].classList.add('active');
+                clearInterval(interval);
+            }
+        }
+        attempts++;
+        if (attempts >= 600) {
+            clearInterval(interval);
+            console.log('Header not found within 1 minute.');
+        }
+    }, 100); 
+}
